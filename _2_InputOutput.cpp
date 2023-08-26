8a66e09b5fd3a765db6ef263df2281c2eabf67d3
Mayank Kumar Singh <77068048+Mayankdjcode@users.noreply.github.com>
2023-08-26 13:19:35 +0530

    Add files via upload

---
+++ _2_InputOutput.cpp
@@ -0,0 +1,12 @@
+// Simple Input Output Program in C++
+#include<iostream>
+using namespace std;
+int main()
+{
+ char ch1,ch2;
+ // The cin and cout operators are chainable
+ cout << "Enter a character and press return: ";
+ cin >> ch1>>ch2;
+ cout <<"Inputs are : "<< ch1<<" "<<ch2;
+ return 0;
+}
